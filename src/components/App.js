0597b9cdf33f8519c418a85609ffa171d785ca8d
Aqil Thanawala <aqil.thanawala@gmail.com>
2019-04-13 17:26:52 -0700

    try merging conflicts

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,15 @@
-<<<<<<< HEAD:src/App.js
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import styles from './App.module.css';
-import ReservationList from './Components/ReservationList';
-=======
 import React, { Component } from "react";
 import logo from "../assets/logo.svg";
 import "..//styles/App.module.css";
 import MainDisplay from "./MainDisplay";
->>>>>>> d5870bc119ab1e8106cabd438513919612735252:src/components/App.js
 
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD:src/App.js
-      <div className={styles.App}>
-        <header className={styles.AppHeader}>
-          <img src={logo} className={styles.AppLogo} alt="logo" />
-=======
       <div className="App">
         <MainDisplay />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
->>>>>>> d5870bc119ab1e8106cabd438513919612735252:src/components/App.js
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -35,7 +22,6 @@ class App extends Component {
             Learn React
           </a>
         </header>
-        <ReservationList />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.module.css";
-import App from "./components/App";
+import App from "./Components/App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
