1eb1d2647764591ff4bf572149ff788130ce0654
Aqil Thanawala <aqil.thanawala@gmail.com>
2019-04-13 17:41:09 -0700

    FIXED SOME DAMN SHIT

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import logo from "../assets/logo.svg";
-import "..//styles/App.module.css";
+import styles from "../styles/App.module.css";
 import MainDisplay from "./MainDisplay";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={styles.App}>
         <MainDisplay />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <header className={styles.AppHeader}>
+          <img src={logo} className={styles.AppLogo} alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
